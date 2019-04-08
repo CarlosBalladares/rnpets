8329ca1fde41f52410746b4f2c4b3a7b9c0de0f0
Carlos Balladares <carlos@balladares.co>
2019-04-08 08:03:15 -0400

    adding

--- App.js
+++ App.js
@@ -1,88 +1,105 @@
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Button, Image } from 'react-native';
-import axios from 'axios';
+import React from "react";
+// import { StyleSheet, Text, View, ScrollView, Button, Image } from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  H1,
+  H2,
+  H3,
+  Text,
+  Button,
+  Body,
+  Title,
+  Icon,
+  Left
+} from "native-base";
+import pf from "petfinder-client";
 
+// import Pet from "./components/Pet";
+import { NativeRouter, Route, Link } from "react-router-native";
+import Expo, { Font } from "expo";
+import Home from "./components/Home";
+import Pet from "./components/Pet";
+import RandomPet from "./components/RandomPet";
 
-function getPetData(pet){
+import axios from "axios";
 
-  // const res = {}
-  const photo = pet.media.photos.photo.find((item)=>{
-    return item['@size']==='x';
-  })
-  const name = Object.values(pet.name)[0];
-  const description = Object.values(pet.description).length?Object.values(pet.description)[0]:"No description available";
-  let breed  = Array.isArray(pet.breeds.breed)?pet.breeds.breed.reduce((acc, s)=>Object.values(s)[0]+",  "+acc , ""):Object.values(pet.breeds.breed)[0];
-
-  breed =  breed.split(',').map(s=>s.trim()).filter(s => s.length).join(", ");
-  const phone = Object.values(pet.contact.phone)[0] || "No phone available";
-  const email = Object.values(pet.contact.email)[0] || "NA";
-  const state = Object.values(pet.contact.state)[0] || "NA";
-  return{photo, name, description, breed ,  phone, state, email}
-
-}
-
+const petfinder = pf({
+  key: "860324afdc30f92b5ccf51f9c50132e5",
+  secret: "1a3830697c127408dd13020c0bcc0df0"
+});
 
 export default class App extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state= {
-      id:3,
-      name:"init",
-      description: 'sample desc',
-      photo: 'https://facebook.github.io/react-native/docs/assets/favicon.png',
-      vis:false,
-    }
+    this.state = { pets: [], loading: true };
+    this.goBack = this.goBack.bind(this);
   }
 
-  componentDidMount(){
-    // console.log("hello")
-
-    var randomurl = "http://api.petfinder.com/pet.getRandom?format=json&key=860324afdc30f92b5ccf51f9c50132e5&animal=dog";
-
-var baseurl = "http://api.petfinder.com/pet.get?format=json&key=860324afdc30f92b5ccf51f9c50132e5"
-    axios.get(randomurl).then((res, rej)=>{
-      if(res.status!==200) return;
-      var id = Object.values(res.data.petfinder.petIds.id)[0];
-      
-      axios.get(baseurl+"&id="+id).then((res, rej)=>{
-        // if(res.status!==200) res.send("err");
-        const pet = res.data.petfinder.pet;
-        this.setState({
-          id, ...getPetData(pet)
-        });
+  componentDidMount() {
+    petfinder.pet
+      .find({
+        output: "full"
+        // location: "Seattle, WA"
       })
-    })
+      .then(data => {
+        let pets;
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        Font.loadAsync({
+          Roboto: require("native-base/Fonts/Roboto.ttf"),
+          Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+          Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+        }).then(e => {
+          this.setState({
+            pets,
+            loading: false
+          });
+        });
+        // this.setState({ loading: false });
+      });
+  }
+  goBack() {
+    this.props.history.goBack();
   }
   render() {
+    const { loading } = this.state;
+
+    if (loading) {
+      return (
+        <Container>
+          <Header />
+          <Content>
+            <H1>Fetching6</H1>
+          </Content>
+        </Container>
+      );
+    }
     return (
-      
-      <View style={styles.container}>
-      <ScrollView>
-        
-
-        <Text style={{fontWeight:"bold"}}>Adopt this puppy {this.state.name} - {this.state.id}</Text>
-        <Text>{this.state.description}</Text>
-        <Text>{this.state.breed}</Text>
-        <Text>{this.state.phone}</Text>
-        <Text>{this.state.email}</Text>
-        <Image           style={{width: 500, height: 500}}
-          source={{uri: this.state.photo['$t']}}
-        ></Image>
-
-
-        </ScrollView>
-      </View>
+      <NativeRouter>
+        <Route path="/pet/:id" component={Pet} />
+        <Route path="/adoptRandom" component={RandomPet} />
+        <Route exact path="/" component={Home} />
+      </NativeRouter>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    'marginTop':100,
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     'marginTop':100,
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
